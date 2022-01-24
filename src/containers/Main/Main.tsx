d344ff806fbf51db8dd76343f0de6f1cb56285c7
Dang Khoa Nguyen <zp-nguyenk@vng.com.vn>
2022-01-24 22:25:25 +0700

    chore - change to yarn & cleanup unused files & install antd

---
+++ src/containers/Main/Main.tsx
@@ -0,0 +1,14 @@
+import { Layout, PageHeader } from "antd";
+import React from "react";
+
+const Main = () => {
+  return (
+    <PageHeader
+      onBack={() => null}
+      title="Main Title"
+      subTitle="This is a subtitle of Main"
+    />
+  );
+};
+
+export default Main;
